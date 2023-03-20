75821a125fcf4b97288185ad0d475e8a235e49f4
Andre-FOliveira <andre.faustino@hotmail.com>
2023-03-20 17:28:38 -0300

    Implementando Geolocalização
    Ajuste para a geolocalização, sistema atualiza a temperatura pela latitude e longitude do usuario

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,8 @@
-
 let chave = "92559c27a7ada31d5a4a69f4ea980e00"
 
 const data = new Date()
 
-const dia = String(data.getDate()).padStart(2, '0' )
+const dia = String(data.getDate()).padStart(2, '0')
 const mes = String(data.getMonth() + 1).padStart(2, '0')
 const ano = data.getFullYear()
 
@@ -18,8 +17,6 @@ const dataAtual = `${dia}/${mes}/${ano} - ${hora}:${min}:${seg}`
 
 function colocarNaTela(dados) {
     console.log(dados)
-    
-    
 
     document.getElementById("data").innerHTML = dataAtual
     document.querySelector(".cidade").innerHTML = dados.name
@@ -27,23 +24,21 @@ function colocarNaTela(dados) {
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
     document.querySelector(".sensacao-termica").innerHTML = Math.floor(dados.main.feels_like) + "°C"
     document.querySelector(".descricao").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML =  dados.main.humidity + "%"
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
     document.querySelector(".acao").src = "./src/animated/" + dados.weather[0].icon + ".svg"
     document.querySelector(".vento").innerHTML = Math.floor(dados.wind.speed * 3.6) + " km/h"
 }
 
 
-
-
 async function buscarCidade(cidade) {
 
     let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
-        cidade +
-        "&appid=" +
-        chave +
-        "&lang=pt_br" +
-        "&units=metric"
-    )
+            cidade +
+            "&appid=" +
+            chave +
+            "&lang=pt_br" +
+            "&units=metric"
+        )
         .then(resposta => resposta.json())
 
 
@@ -51,14 +46,50 @@ async function buscarCidade(cidade) {
 
 }
 
+
 function pesquisa() {
     let cidade = document.querySelector(".input-city").value.trim();
 
     buscarCidade(cidade)
 }
 
+window.addEventListener('load', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setPosition, showError);
+    } else {
+        alert('navegador não suporta geolozalicação');
+    }
+
+    function setPosition(position) {
+        console.log(position)
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        coordResults(lat, long);
+    }
+
+    function showError(error) {
+        alert(`erro: ${error.message}`);
+    }
+})
+
+function coordResults(lat, long) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&lang=pt_br&units=metric&APPID=${chave}`)
+        .then(resp => {
+            if (!resp.ok) {
+                throw new Error(`http error: status ${resp.status}`)
+            }
+            return resp.json();
+        })
+        .catch(error => {
+            alert(error.message)
+        })
+
+        .then(resp => {
+            colocarNaTela(resp)
+        });
+}
 
 
 function mudaTema() {
     document.body.classList.toggle("dark");
-  }
+}
